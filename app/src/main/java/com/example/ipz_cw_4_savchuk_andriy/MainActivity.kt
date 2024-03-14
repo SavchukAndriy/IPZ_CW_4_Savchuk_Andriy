d0a223b98810635369963167596a6c85d3fbfa91
Savchuk <100482478+abdabdab@users.noreply.github.com>
2024-03-14 14:37:25 +0200

    Create function TaskListScreen to create and show TopBar, that will output text

--- app/src/main/java/com/example/ipz_cw_4_savchuk_andriy/MainActivity.kt
+++ app/src/main/java/com/example/ipz_cw_4_savchuk_andriy/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.ipz_cw_4_savchuk_andriy
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.example.ipz_cw_4_savchuk_andriy.ui.theme.IPZ_CW_4_Savchuk_AndriyTheme
 
@@ -31,3 +37,16 @@ enum class TaskStatus {
 }
 
 data class Task(val title: String, val status: TaskStatus, val description: String, val date: String)
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TaskListScreen(tasks: List<Task>, onItemClick: (Task) -> Unit) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Список завдань") }
+            )
+        }
+    )
+}
