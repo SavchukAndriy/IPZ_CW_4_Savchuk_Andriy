54c067a00ac724e1eef7ebae1c4da0bf0d600ca4
Savchuk <100482478+abdabdab@users.noreply.github.com>
2024-03-14 14:32:32 +0200

    create class TaskStatus to show status different functions

--- app/src/main/java/com/example/ipz_cw_4_savchuk_andriy/MainActivity.kt
+++ app/src/main/java/com/example/ipz_cw_4_savchuk_andriy/MainActivity.kt
@@ -3,13 +3,10 @@ package com.example.ipz_cw_4_savchuk_andriy
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.ipz_cw_4_savchuk_andriy.ui.theme.IPZ_CW_4_Savchuk_AndriyTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,4 +24,8 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+
+enum class TaskStatus {
+    ACTIVE, DONE
 }
